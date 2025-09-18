5ee06f9532fba3b2fe75ffde374bd1628e49af91
Ryan Luong <lumintriet@gmail.com>
2025-09-18 09:51:10 +0700

    Configure Clerk components' theme.

--- src/components/providers/clerk-client.tsx
+++ src/components/providers/clerk-client.tsx
@@ -1,9 +1,15 @@
 import { ClerkProvider } from "@clerk/clerk-react";
+import { shadcn } from "@clerk/themes";
 import { viteEnv } from "@/lib/vite-env";
 
 function ClerkClientProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider publishableKey={viteEnv.VITE_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={viteEnv.VITE_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        baseTheme: shadcn
+      }}
+    >
       {children}
     </ClerkProvider>
   );
