bf8d41c51c391ad532e2a50e1df8998ff757266d
Ryan Luong <lumintriet@gmail.com>
2025-09-18 08:06:15 +0700

    Add mode toggle and loading spinner components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/utils/mode-toggle";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button onClick={() => toast.info("Hello")}>Click me</Button>
+      <ModeToggle className="fixed right-4 bottom-4" />
     </div>
   );
 }
