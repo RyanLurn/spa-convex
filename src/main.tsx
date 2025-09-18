09363e7b315fa25733e7a29ed02724564b9a0138
Ryan Luong <lumintriet@gmail.com>
2025-09-18 08:25:49 +0700

    Configure TanStack Router.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { scan } from "react-scan";
 // must be imported before React and React DOM
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "@/App.tsx";
 import { Providers } from "@/components/providers";
 import "@/index.css";
 
@@ -12,8 +11,6 @@ scan({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Providers>
-      <App />
-    </Providers>
+    <Providers />
   </StrictMode>
 );
